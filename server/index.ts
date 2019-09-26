dbfd6c26a8ede0f08b31ca22ab596210597ef79b
James <james.smith@heig-vd.ch>
2019-09-26 09:53:25 +0200

    Server with user and events. Client project created

---
+++ server/index.ts
@@ -0,0 +1,40 @@
+import { prisma } from './generated/prisma-client'
+import datamodelInfo from './generated/nexus-prismas'
+import * as path from 'path'
+import { stringArg, idArg } from 'nexus'
+import { prismaObjectType, makePrismaSchema } from 'nexus-prisma'
+import { GraphQLServer } from 'graphql-yoga'
+
+const Query = prismaObjectType({
+  name: 'Query',
+  definition(t) {
+    t.prismaFields(['*'])
+  },
+})
+
+const Mutation = prismaObjectType({
+  name: 'Mutation',
+  definition(t) {
+    t.prismaFields(['*'])
+  },
+})
+
+const schema = makePrismaSchema({
+  types: [Query, Mutation],
+
+  prisma: {
+    datamodelInfo,
+    client: prisma,
+  },
+
+  outputs: {
+    schema: path.join(__dirname, './generated/schema.graphql'),
+    typegen: path.join(__dirname, './generated/nexus.ts'),
+  },
+})
+
+const server = new GraphQLServer({
+  schema,
+  context: { prisma },
+})
+server.start(() => console.log('Server is running on http://localhost:4000'))
